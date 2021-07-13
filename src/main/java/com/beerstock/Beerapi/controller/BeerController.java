c591497431663c0fdb9723e77d8fac25a31a6913
dannilo <dannilo-pires@hotmail.com>
2021-07-13 09:33:01 -0300

    minor changes, trivia about beer types

--- src/main/java/com/beerstock/Beerapi/controller/BeerController.java
+++ src/main/java/com/beerstock/Beerapi/controller/BeerController.java
@@ -1,4 +1,15 @@
 package com.beerstock.Beerapi.controller;
 
-public class BeerController {
+import com.beerstock.Beerapi.service.BeerService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/beers")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class BeerController implements BeerControllerDocs {
+
+    private final BeerService beerService;
 }
